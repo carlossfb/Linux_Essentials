18f78a36615923e78969e9a3307580fe68bdd14c
Carlos Eduardo <72090014+carlossfb@users.noreply.github.com>
2022-08-19 18:08:19 -0300

    projeto apache

---
+++ Scripts2/iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 unzip -y
+
+echo "Baixando e copiando arquivos da aplicacao..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -r * /var/www/html/
+
+echo "Finalizado..."
